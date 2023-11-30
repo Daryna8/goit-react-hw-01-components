52af1c2d286e0c7c5bceb592f5a3e668b2c2c676
Daryna8 <142911382+Daryna8@users.noreply.github.com>
2023-11-30 22:26:09 +0200

    created components without style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,13 @@
+import { Profile } from './Profile.jsx';
+import { Statistics } from './Statistics.jsx';
+import { FriendList } from './FriendList.jsx';
+import { TransactionHistory } from './TransactionHistory.jsx';
+
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +17,20 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     </div>
   );
 };
